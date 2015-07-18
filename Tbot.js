ba7d9b152e0607177d30014bde9fd3f4f2eae7e9
ksami <ksami.ihide@gmail.com>
2015-07-18 23:27:17 +0800

    refactor into class

---
+++ Tbot.js
@@ -0,0 +1,102 @@
+var request = require('request');
+
+function Tbot(config, parsingFunction){
+    var self = this;
+
+    this.URL = config.url;
+    this.TOKEN = config.token;
+    this.TIMEOUT = config.timeout;
+    this.QUIET = config.isQuiet;
+
+    this._updatesOffset = 0;
+    this._chatId = '';
+
+    //function for parsing the message
+    this.parse = parsingFunction;
+    //state object for storing state
+    this.state = {num: 0};
+
+    // Request to Telegram API
+    //params:
+    //method - one of Telegram's Bot API methods
+    //chatIdStr - chatId as '?chat_id=' or offset as '?offset='
+    //param - any other params eg. '&text='
+    //callback - callback function
+    this.sendRequest = function(method, chatIdStr, param, callback){
+        var cb = callback || function(body){ if(!self.QUIET) console.log(body); };
+        var req = self.URL + self.TOKEN + '/' + method + chatIdStr + param;
+
+        request({
+            method: 'GET',
+            url: req,
+            json: true
+        }, function(err, res, body){
+            try{
+                if(!err && res.statusCode == 200){
+                    if(body.ok){               
+                        cb(body);                        
+                    }
+                    else{
+                        throw new Error('body not ok');
+                    }   
+                }
+                else{
+                    if(err){
+                        throw new Error(err.toString());
+                    }
+                    else{
+                        throw new Error('response code is ' + res.statusCode);
+                    }
+                }
+            }
+            catch(e){
+                console.log('Error: ' + e.message);
+            }
+        });
+    };
+
+    // Sends a message for each text message received
+    //params:
+    //body - array of Update objects
+    this.processUpdates = function(body){
+        for (var i = 0; i < body.result.length; i++) {
+            var update = body.result[i];
+
+            if(update.update_id >= self._updatesOffset){
+                self._updatesOffset = update.update_id + 1;
+            }
+
+            if(update.hasOwnProperty('message')){
+                self._chatId = update.message.chat.id;
+
+                if(update.message.hasOwnProperty('text')){
+                    var reply = self.parse(update.message.text);
+                    self.sendMessage(reply);
+                }
+                else{
+                    throw new Error('no text');
+                }
+            }
+            else{
+                //no message
+            }
+
+        }
+    };
+}
+
+
+Tbot.prototype.getMe = function(callback){
+    this.sendRequest('getMe', '', '', callback);
+};
+
+Tbot.prototype.getUpdates = function(){
+    this.sendRequest('getUpdates', '', '?offset='+this._updatesOffset, this.processUpdates);
+};
+
+Tbot.prototype.sendMessage = function(msg){
+    this.sendRequest('sendMessage', '?chat_id='+this._chatId, '&text='+msg);
+};
+
+
+module.exports = Tbot;
--- app.js
+++ app.js
@@ -8,148 +8,13 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 // Dependencies
 var config = require((process.env.OPENSHIFT_DATA_DIR || __dirname) + '/.config/config.json');
-var request = require('request');
+var Tbot = require('./Tbot');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
 // Use files from folder 'www'
 // app.use(express.static('www'));
-
-var URL = config.url;
-var TOKEN = config.token;
-var TIMEOUT = config.timeout;
-var QUIET = config.isQuiet;
-
-var _updatesOffset = 0;
-var _chatId = '';
-
-
-
-//execute
-setInterval(function(){
-    getUpdates(processUpdates);
-}, TIMEOUT*1000);
-
-
-
-
-
-
-function sendMessage(msg){
-    requestTelegram('sendMessage', '?chat_id='+_chatId, '&text='+msg);
-}
-
-
-function getUpdates(callback){
-    requestTelegram('getUpdates', '', '?offset='+_updatesOffset, callback);
-}
-
-
-function getMe(callback){
-    requestTelegram('getMe', '', '', function(){
-        console.log(body);
-    });
-}
-
-
-function parse(msg){
-    var returnMessage = '';
-
-    if(typeof msg !== "undefined" && msg.charAt(0) === '/') {
-        text = msg.split(' ');
-        command = text[0];
-
-        if(command == '/start' || command == '/help'){
-            returnMessage += '/roll x - to obtain a number between 1 and x (eg. /roll 6)\n';
-            returnMessage += '/help to see this message again';
-        }
-        else if(command == '/ksami'){
-            returnMessage = 'ksami is cool, ksami is great, ksami is rated ten out of eight';
-        }
-        else if(command == '/haha'){
-            returnMessage = 'Haha yourself';
-        }
-        else if(command == '/roll'){
-            var max = parseInt(text[1]);
-
-            if(!isNaN(max) && max >= 1){
-                returnMessage = 'Rolling a ' + max + '-sided die...\n';
-                returnMessage += 'Result is: ';
-                returnMessage += (Math.floor((Math.random() * max) + 1)).toString();
-            }
-            else{
-                returnMessage = 'Please enter a number more than 0 (eg. /roll 6)';
-            }
-        }
-    }
-    return returnMessage;
-}
-
-
-function processUpdates(body){
-    for (var i = 0; i < body.result.length; i++) {
-        var update = body.result[i];
-
-        if(update.update_id >= _updatesOffset){
-            _updatesOffset = update.update_id + 1;
-        }
-
-        if(update.hasOwnProperty('message')){
-            _chatId = update.message.chat.id;
-
-            if(update.message.hasOwnProperty('text')){
-                var reply = parse(update.message.text);
-                sendMessage(reply);
-            }
-            else{
-                throw new Error('no text');
-            }
-        }
-        else{
-            //no message
-        }
-
-    }
-}
-
-
-function requestTelegram(method, chatIdStr, param, callback){
-    var cb = callback || function(body){ if(!QUIET) console.log(body); };
-    var req = URL + TOKEN + '/' + method + chatIdStr + param;
-
-    // Request to Telegram API
-    request({
-        method: 'GET',
-        url: req,
-        json: true
-    }, function(err, res, body){
-        try{
-            if(!err && res.statusCode == 200){
-                if(body.ok){
-                    
-                    cb(body);
-                    
-                }
-                else{
-                    throw new Error('body not ok');
-                }   
-            }
-            else{
-                if(err){
-                    throw new Error(err.toString());
-                }
-                else{
-                    throw new Error('response code is ' + res.statusCode);
-                }
-            }
-        }
-        catch(e){
-            console.log('Error: ' + e.message);
-        }
-    });
-}
-
 
 // Listen to <port>
 http.listen(port, ipaddress, function(){
@@ -160,3 +25,45 @@ http.listen(port, ipaddress, function(){
 app.get('/',function(req, res){
     res.send('Hi there!');
 });
+
+
+// Execute
+var ksamiBot = new Tbot(config, function(msg){
+    var returnMessage = '';
+    //debug
+    this.state.num += 1;
+    returnMessage += this.state.num + ':\n';
+
+    if(typeof msg !== "undefined" && msg.charAt(0) === '/') {
+        text = msg.split(' ');
+        command = text[0];
+
+        if(command == '/start' || command == '/help'){
+            returnMessage += '/roll x - to obtain a number between 1 and x (eg. /roll 6)\n';
+            returnMessage += '/help to see this message again';
+        }
+        else if(command == '/ksami'){
+            returnMessage += 'ksami is cool, ksami is great, ksami is rated ten out of eight';
+        }
+        else if(command == '/haha'){
+            returnMessage += 'Haha yourself';
+        }
+        else if(command == '/roll'){
+            var max = parseInt(text[1]);
+
+            if(!isNaN(max) && max >= 1){
+                returnMessage += 'Rolling a ' + max + '-sided die...\n';
+                returnMessage += 'Result is: ';
+                returnMessage += (Math.floor((Math.random() * max) + 1)).toString();
+            }
+            else{
+                returnMessage += 'Please enter a number more than 0 (eg. /roll 6)';
+            }
+        }
+    }
+    return returnMessage;
+});
+
+setInterval(function(){
+    ksamiBot.getUpdates();
+}, ksamiBot.TIMEOUT*1000);
